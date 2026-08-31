dc726ef468c6974cdad44faaef5904e592e06cc9
zeupex3 <zeupex666@gmail.com>
2026-08-31 19:29:51 +0700

    Add Favorite Bands Kub

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.genius.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
